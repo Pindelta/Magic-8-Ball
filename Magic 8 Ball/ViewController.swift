0e48950a82f3b21dccc024b041d337969ef315f7
Osmani Perez <Osmaniperez1999@yahoo.com>
2019-08-29 12:02:39 -0500

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Osmani Perez on 8/29/19.
+//  Copyright © 2019 Osmani Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
